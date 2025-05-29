e7082aa9dc4d91f13fed13aa0a41ea93792c28d3
Imran Shah <imranshah10140@gmail.com>
2025-05-29 21:32:02 +0530

    Updated the Prisma and Added the User

--- prisma/migrations/20250520184330_addexplanation/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "ManimProject" ADD COLUMN     "explanation" TEXT;
--- prisma/migrations/20250529004219_init/migration.sql
+++ prisma/migrations/20250529004219_init/migration.sql
@@ -3,6 +3,7 @@ CREATE TABLE "ManimProject" (
     "id" TEXT NOT NULL,
     "prompt" TEXT NOT NULL,
     "code" TEXT NOT NULL,
+    "explanation" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -16,12 +17,28 @@ CREATE TABLE "Video" (
     "fileType" TEXT NOT NULL,
     "fileSize" INTEGER NOT NULL,
     "data" BYTEA NOT NULL,
+    "s3Key" TEXT NOT NULL,
+    "s3Bucket" TEXT NOT NULL,
     "isOutput" BOOLEAN NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "manimProjectId" TEXT NOT NULL,
 
     CONSTRAINT "Video_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- AddForeignKey
 ALTER TABLE "Video" ADD CONSTRAINT "Video_manimProjectId_fkey" FOREIGN KEY ("manimProjectId") REFERENCES "ManimProject"("id") ON DELETE CASCADE ON UPDATE CASCADE;
