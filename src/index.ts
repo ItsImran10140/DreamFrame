d1e55ab35c4ec1a1a131a1a016e3507651a2830a
Imran Shah <imranshah10140@gmail.com>
2025-05-30 14:13:50 +0530

    testing source deploy.sh file

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/manim", manimRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Welcome to the Manim API");
+  res.send("Welcome to the Manim API NEW!");
 });
 
 // Connect to database
